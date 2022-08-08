772f5a2177b0e0439eeb310b8cea58960c2e2d3f
Ankit Kumar Singh <Ankit Kumar Singh@192.168.0.102>
2022-08-08 17:11:15 +0530

    commit

---
+++ com.sorting/src/com/sorting/BubbleSort.java
@@ -0,0 +1,51 @@
+package com.sorting;
+
+import java.util.Scanner;
+
+//create SortBubble class.
+class SortBubble{
+	//create bulbbleSort methods.
+	  static int [] bubbleSort(int [] array) {
+		
+		int size=array.length;
+		for(int i=0;i<size;i++) {
+			for(int j=0;j<size-i-1;j++) {
+				if(array[j]>array[j+1]) {
+					int temp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
+				}
+			}
+		} 
+		return array; 
+	}
+}
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create object of Scanner class to take inputs from user.
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter size of an array");
+		int sizeOfArray=scan.nextInt();
+		//declare a Array.
+		int array[]=new int [sizeOfArray];
+		//take input from user to fill the array by help of for loop.
+		for(int i=0;i<sizeOfArray;i++) {
+			System.out.println("Enter elements of index "+i+" :");
+			array[i]=scan.nextInt();
+		}
+		/*
+		 If the method is not Static then we call the method by creating the object of a class.
+		 Sort object1=new Sort();
+		 int result[]=object1.bubbleSort(array);
+		 */
+		
+		//call the method bubbleSort().
+		int []result=SortBubble.bubbleSort(array);
+		for(int i=0;i<sizeOfArray;i++)System.out.println(result[i]);
+		scan.close();
+	}
+
+}
---
+++ com.sorting/src/com/sorting/BubbleSortOptimize.java
@@ -0,0 +1,53 @@
+package com.sorting;
+import java.util.Scanner;
+
+//create class SortBubbleOptimize
+class SortBubbleOptimize{
+	//Create method bubbleSortOptimize().
+	static int [] bubbleSortOptimize(int [] array) {
+		//size of array.
+		int size=array.length;
+		//declare flag to check array are sorted from particular index.
+		boolean flag=true;
+		//loop for sort an arrays.
+		for(int i=0;i<size && flag==true;i++) {
+			flag=false;
+			for(int j=0;j<size-1-i;j++) {
+				if(array[j]>array[j+1]) {
+					int temp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
+					flag=true;
+				}
+			}
+		} 
+		return array;
+	}
+}
+
+public class BubbleSortOptimize {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create object of Scanner class.
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter size of an array: ");
+		//declare variable for size of an arrays.
+		int sizeOfArray=scan.nextInt();
+		//declare an array.
+		int array[]=new int [sizeOfArray];
+		//insert values of an array by taking input from user with the help of for loop.
+		for(int i=0;i<sizeOfArray;i++) {
+			System.out.println("Enter element of array at index"+i+" :");
+			array[i]=scan.nextInt();
+		}
+		//call the method bubbleSortOptimize.
+		int [] result=SortBubbleOptimize.bubbleSortOptimize(array);
+		//print the sorted array.
+		for(int i=0;i<sizeOfArray;i++) {
+			System.out.print(result[i]+" ");
+		}
+		
+	}
+
+}
